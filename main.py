12fb86283f0dedcf31e72c594c97915ec5481210
mini-M-py <dhakalm909@gmail.com>
2023-05-28 08:40:28 +0545

    remove you from js

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            #await manager.send_personal_message(f"You wrote: {data}", websocket)
             await manager.broadcast(f"Client #{client_id}: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
