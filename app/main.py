26935d26d9dc4b9a2dbad7828b2b84574eda8d6e
mini-M-py <dhakalm909@gmail.com>
2023-07-05 13:27:35 +0545

    custome middle ware to handle 401 error

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import json
 from .routers import chat, user, auth
-from fastapi import FastAPI, Request, Response, WebSocket, WebSocketDisconnect, Depends
+from fastapi import FastAPI, Request, Response, WebSocket, WebSocketDisconnect, Depends, status, HTTPException
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from fastapi.exception_handlers import http_exception_handler
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from . import model, oauth2
@@ -24,13 +25,26 @@ app.include_router(user.router)
 app.include_router(auth.router)
 model.Base.metadata.create_all(bind=engine)
 
+# Custom exception handler for 401 Unauthorized
+async def unauthorized_exception_handler(request, exc):
+    if exc.status_code == status.HTTP_401_UNAUTHORIZED:
+        return RedirectResponse(url="/Login")
 
+    # Call the default exception handler for other cases
+    return await http_exception_handler(request, exc)
+
+# Register the custom exception handler
+app.exception_handler(HTTPException)(unauthorized_exception_handler)
+
+#root
 @app.get("/")
 async def get(request: Request,  user_id: int = Depends(oauth2.get_current_user)):
     extensions = request.query_params.get("extensions", {})
     return templates.TemplateResponse("chat.html", {"request": request, "extensions": extensions})
+#login page
+@app.get('/Login')
+async def get(request: Request):
+    extensions = request.query_params.get('extensions', {})
+    return templates.TemplateResponse("login.html", {"request":request, "extensions":extensions})
 
-@app.get("/favicon.ico")
-async def favicon():
-    return Response(status_code=204)
 
--- app/oauth2.py
+++ app/oauth2.py
@@ -34,6 +34,6 @@ def verify_access_token(token: str, credential_exception):
         raise credential_exception
     return token_data
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"could not validate credentials",
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"unauthorized",
                                           headers={"WWW-Authenticate":"Bearer"})
     return verify_access_token(token, credentials_exception)
